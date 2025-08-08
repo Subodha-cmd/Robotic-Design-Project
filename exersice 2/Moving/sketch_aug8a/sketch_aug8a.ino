8ae2afef4b1effe437675790a6e802a6dc84f51e
Subodha <subodhaishere@gmail.com>
2025-08-08 19:30:31 +0530

    update moving library

--- exersice 2/Moving/sketch_aug8a/sketch_aug8a.ino
+++ exersice 2/Moving/sketch_aug8a/sketch_aug8a.ino
@@ -1,22 +1,18 @@
-#include <Servo.h>
-
-Servo servo;  // Servo on port 1 of the motor shield
+#include "velocity.h"
 
 void setup() {
-  servo.attach(52);   // Attach servo to port 1
-  servo.write(90);   // Move servo to center position (90 degrees)
+  pinMode(CLK, INPUT);
+  attachInterrupt(digitalPinToInterrupt(CLK), encoderISR, RISING);
+
+  Serial.begin(9600);
+  lastMeasureTime = millis();
 }
 
 void loop() {
-  // Sweep servo from 0 to 180 degrees
-  for (int pos = 0; pos <= 180; pos++) {
-    servo.write(pos);
-    delay(15);
-  }
-
-  // Sweep servo back from 180 to 0 degrees
-  for (int pos = 180; pos >= 0; pos--) {
-    servo.write(pos);
-    delay(15);
+  float angularVelocity = getAngularVelocity();  // rad/s
+  if (angularVelocity >= 0) {
+    Serial.print("Angular Velocity: ");
+    Serial.print(angularVelocity);
+    Serial.println(" rad/s");
   }
 }
---
+++ exersice 2/Moving/sketch_aug8a/velocity.h
@@ -0,0 +1,60 @@
+#ifndef LINE_FOLLOW_H
+#define LINE_FOLLOW_H
+
+#define CLK 21 // Interrupt-capable pin
+#define PPR 30  // Pulses per revolution
+#include <Arduino.h>
+
+volatile long pulseCount = 0;
+unsigned long lastMeasureTime = 0;
+
+
+float getAngularVelocity() {
+  unsigned long currentTime = millis();
+  float deltaTime = (currentTime - lastMeasureTime) / 1000.0; // seconds
+
+  if (deltaTime >= 0.1) {  // Calculate every 100 ms
+    noInterrupts();
+    long count = pulseCount;
+    pulseCount = 0;
+    interrupts();
+
+    float revolutions = (float)count / PPR;
+    float omega = (revolutions / deltaTime) * 2.0 * PI;  // rad/s
+
+    lastMeasureTime = currentTime;
+    return omega;
+  }
+
+  return -1.0;  // Not ready yet
+}
+void encoderISR() {
+
+  pulseCount++;
+  }
+
+
+/*
+
+sample code here
+
+void setup() {
+  pinMode(CLK, INPUT);
+  attachInterrupt(digitalPinToInterrupt(CLK), encoderISR, RISING);
+
+  Serial.begin(9600);
+  lastMeasureTime = millis();
+}
+
+void loop() {
+  float angularVelocity = getAngularVelocity();  // rad/s
+  if (angularVelocity >= 0) {
+    Serial.print("Angular Velocity: ");
+    Serial.print(angularVelocity);
+    Serial.println(" rad/s");
+  }
+}
+
+*/
+
+#endif
